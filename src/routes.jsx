e9ee7e3e2888978006158d8ee8c56d91bfb0b5d6
focus7eleven <441002295@qq.com>
2016-12-26 01:00:42 +0800

    :fist: add editor :fist:

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {Router, Route, browserHistory, IndexRoute} from 'react-router'
 import AppContainer from './containers/AppContainer'
 import HelloKdotContainer from './containers/HelloKdotContainer'
+import AnnouncementEditor from './containers/editor/AnnouncementEditor'
 
 const routes = (
 	<Router history={browserHistory}>
 		<Route path="/" component={AppContainer}>
 			<Route path="test">
 				<Route path="kdot" component={HelloKdotContainer}></Route>
+				<Route path="editor" component={AnnouncementEditor}></Route>
 			</Route>
 		</Route>
 	</Router>
