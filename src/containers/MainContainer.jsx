e6a9b41e267703acab8eb490ad96c255c853f9e1
xuyangdong <804954666@qq.com>
2016-12-28 21:38:16 +0800

    modified:   src/containers/MainContainer.jsx 	modified:   src/containers/NoticeManagerContainer.jsx

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -4,8 +4,9 @@ import Navigate from '../components/Navigate'
 import LeftBoard from '../components/LeftBoard'
 import {Affix,Icon} from 'antd'
 import logo from '../../resource/3.jpg'
+import {withRouter} from 'react-router'
 
-const MainContainer = React.createClass({
+const MainContainer = withRouter(React.createClass({
   getInitialState(){
     return {
       showMenuBar:false,
@@ -22,6 +23,14 @@ const MainContainer = React.createClass({
       showLeftBoard:!this.state.showLeftBoard
     })
   },
+  componentWillReceiveProps(nextProps){
+    console.log("---->:",nextProps)
+    if(nextProps.location.pathname == '/index/base-info'){
+      this._menuList = [{'基础信息':['学段','年级','学科']},{'组织结构':['学校机构','年级管理']},{'人员管理':['老师','学生','家长']},{'群组管理':['通用群','定制群']},{'教学大纲':['教学大纲']},{'工具管理':['健康档案','课程表','菜谱']}]
+    }else if(nextProps.location.pathname == '/index/notice-mgr'){
+      this._menuList = [{'通知':['新建的通知','收到的通知','发送的通知']}]
+    }
+  },
   render(){
     return (
       <div style={{height:'100%',width:'100%'}}>
@@ -31,7 +40,7 @@ const MainContainer = React.createClass({
             <Navigate />
           </div>
           <div className={styles.content}>
-            <div className={styles.leftBoard}><LeftBoard /></div>
+            <div className={styles.leftBoard}><LeftBoard menuList={this._menuList}/></div>
             <div className={styles.workspace}>
             {
               this.props.children
@@ -47,7 +56,7 @@ const MainContainer = React.createClass({
           <div className={styles.content}>
             <div className={styles.leftBoardContainer} style={this.state.showLeftBoard?{transform:'translateX(0px)',transition:'transform 0.7s'}:{transform:'translateX(-200px)',transition:'transform 0.7s'}}>
                 <div className={styles.toggleButton}>{this.state.showLeftBoard?<Icon type="menu-fold" onClick={this.handleShowLeftBoard} style={{fontSize:'24px'}}/>:<Icon type="menu-unfold" onClick={this.handleShowLeftBoard} style={{fontSize:'24px'}}/>}</div>
-                <div className={styles.leftBoard}><LeftBoard /></div>
+                <div className={styles.leftBoard}><LeftBoard menuList={this._menuList}/></div>
             </div>
             <div className={styles.workspace}>
               {
@@ -59,6 +68,6 @@ const MainContainer = React.createClass({
       </div>
     )
   }
-})
+}))
 
 export default MainContainer
--- src/containers/NoticeManagerContainer.jsx
+++ src/containers/NoticeManagerContainer.jsx
@@ -56,8 +56,8 @@ const NoticeManagerContainer = React.createClass({
   },
   getInitialState(){
     return {
-      columns:[],
-      data:[],
+      columns:COLUMNS,
+      data:DATA,
       showNavigate:false,
       showLeftBoard:false,
     }
@@ -91,37 +91,15 @@ const NoticeManagerContainer = React.createClass({
   render(){
     const {columns,data} = this.state
     return (
-      <div style={{height: '100%'}}>
-        <Row>
-          <Col>
-            <div className={styles.navigateBar}><Navigate /></div>
-            <div className={styles.miniNavigateBar}>
-              <Icon type="bars" style={{fontSize:'24px'}} onClick={this.handleDisplayNavigate}/>
-              <div className={styles.miniNavigate} style={this.state.showNavigate?{display:'block'}:{display:'none'}}><Navigate mode='inline'/></div>
-            </div>
-          </Col>
-        </Row>
-        <div style={{height: 'calc( 100% - 48px )'}}>
-          <Row type='flex'  gutter={16} style={{height:'100%'}}>
-            <Col span={24} className={styles.content}>
-              <div className={styles.plainLeftBoard}>
-                <LeftBoard menuList={this.props.menuList} onSelect={this.handleClick}/>
-              </div>
-              <div className={styles.leftBoard} style={this.state.showLeftBoard?{width:'200px'}:{width:'30px'}}>
-                <div className={styles.miniLeftBoardToggler}><Icon type="bars" style={{fontSize:'24px'}} onClick={this.handleDisplayLeftBoard}/></div>
-                <div className={styles.miniLeftBoard} style={this.state.showLeftBoard?{display:'block',width:'200px'}:{display:'none'}}><LeftBoard menuList={this.props.menuList} onSelect={this.handleClick}/></div>
-              </div>
-              <div className={styles.tableContent}>
-                {
-                  columns?<Table columns={columns} dataSource={data} />:null
-                }
-              </div>
-            </Col>
-
-          </Row>
-        </div>
-      </div>
-
+      <Row type='flex'  gutter={16} style={{height:'100%'}}>
+        <Col span={24} className={styles.content}>
+          <div className={styles.tableContent}>
+            {
+              columns?<Table columns={columns} dataSource={data} />:null
+            }
+          </div>
+        </Col>
+      </Row>
     )
   }
 })
