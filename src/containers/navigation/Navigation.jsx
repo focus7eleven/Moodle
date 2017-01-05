d662bd1f0ca085daba977b8b40cf9e9098bfcf62
focus7eleven <441002295@qq.com>
2017-01-05 23:55:34 +0800

    navigation layout updated

--- src/containers/navigation/Navigation.jsx
+++ src/containers/navigation/Navigation.jsx
@@ -6,7 +6,7 @@ import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import logo from 'images/logo.png'
 import {getMenu} from '../../actions/menu'
-import {setPath} from '../../actions/workspace'
+import classNames from 'classnames'
 
 const Navigation = React.createClass({
   contextTypes: {
@@ -16,12 +16,29 @@ const Navigation = React.createClass({
   getInitialState(){
     return {
       openSubMenu:false,
+      // openSubMenu:true,
       currentMenu:'',
+      menuHeight: 250,
+      minThirdMenuCount: 5,
     }
   },
+
   componentDidMount(){
     this.props.user.get('accessToken')?this.props.getMenu(this.props.user.get('accessToken')):null
   },
+
+  componentWillReceiveProps(nextProps){
+    console.log(nextProps.menu.get('data').toJS());
+    let min = 5;
+    nextProps.menu.get('data').map(item=>{
+      item.get('childResources').map(item=>{
+        min =  item.get('childResources').size > min ? item.get('childResources').size : min;
+      })
+    })
+    const newMenuHeight = min > 5 ? 250 + (min - 5) * 40 : 250;
+    this.setState({minThirdMenuCount:min,menuHeight:newMenuHeight});
+  },
+
   handleDropDownSubmenu(key){
     this.setState({
       currentMenu:key,
@@ -36,11 +53,7 @@ const Navigation = React.createClass({
   },
 
   handleSelectMenu(e){
-    const subMenu = e.target.getAttribute('data-subMenu');
-    const second = e.target.getAttribute('data-second');
-    const third = e.target.getAttribute('data-third');
     const url = e.target.getAttribute('data-url');
-    // this.props.setPath([subMenu,second,third]);
     this.setState({
       openSubMenu:false,
     })
@@ -50,30 +63,35 @@ const Navigation = React.createClass({
   renderSubMenu(){
     const {currentMenu} = this.state
     const subMenu = currentMenu?this.props.menu.get('data').findEntry( v => v.get('resourceUrl')==currentMenu)[1]:null
+    const isVertical = subMenu?subMenu.get('childResources').size>3:true;
     return (
+      subMenu?
       <div className={styles.dropDownContainer}>
-        <div className={styles.panelContent}>
+        <div style={{height:this.state.menuHeight-30+"px"}} className={classNames(isVertical?styles.panelContent:styles.panelContentHori)}>
         {
-          subMenu?subMenu.get('childResources').map( (second,key) => {
+          subMenu.get('childResources').map( (second,key) => {
             return (
-              <div key={key} className={styles.secondMenu}>
-                <div className={styles.secondMenuTitle}><span>{second.get('resourceName')}</span><Icon style={{color:'rgb(80,80,80)'}} type="right" /></div>
-                {
-                  second.get('childResources').map( third => {
-                    return (
-                      <div data-url={third.get('resourceUrl')} data-subMenu={subMenu.get('resourceName')} data-second={second.get('resourceName')} data-third={third.get('resourceName')} key={third.get('resourceName')} className={styles.thirdMenu} onClick={this.handleSelectMenu}>
-                        {third.get('resourceName')}
-                      </div>
-                    )
-                  })
-                }
+              <div key={key} className={classNames(isVertical?styles.secondMenu:styles.secondMenuHori)}>
+                <div className={classNames(isVertical?styles.secondMenuTitle:styles.secondMenuTitleHori)}><span>{second.get('resourceName')}</span><Icon style={{color:'rgb(80,80,80)'}} type="right" /></div>
+                <div className={classNames(isVertical?null:styles.thirdMenuHoriWrapper)}>
+                  {
+                    second.get('childResources').map( third => {
+                      return (
+                        <div data-url={third.get('resourceUrl')} key={third.get('resourceName')} className={classNames(isVertical?styles.thirdMenu:styles.thirdMenuHori)} onClick={this.handleSelectMenu}>
+                          {third.get('resourceName')}
+                        </div>
+                      )
+                    })
+                  }
+                </div>
               </div>
             )
-          }):null
+          })
         }
         </div>
         <div className={styles.foldIcon}><Icon onClick={this.handleFoldSubmenu} type="up" /></div>
       </div>
+      :null
     )
   },
 
@@ -95,7 +113,7 @@ const Navigation = React.createClass({
           </Menu>
           <div className={styles.avatar}><img src='https://unsplash.it/25/25' /><span className={styles.nameDesc}>曹老师（任课老师）</span></div>
         </div>
-        <Motion defaultStyle={{x: 0}} style={this.state.openSubMenu?{x:spring(250)}:{x:spring(0)}}>
+        <Motion defaultStyle={{x: 0}} style={this.state.openSubMenu?{x:spring(this.state.menuHeight)}:{x:spring(0)}}>
           {interpolatingStyle => (
             <div className={styles.dropDownPanel} style={{height:interpolatingStyle.x+'px'}} onMouseLeave={this.handleFoldSubmenu}>
               {this.renderSubMenu()}
@@ -117,7 +135,6 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch){
   return {
     getMenu:bindActionCreators(getMenu,dispatch),
-    setPath:bindActionCreators(setPath,dispatch),
   }
 }
 export default connect(mapStateToProps,mapDispatchToProps)(Navigation)
